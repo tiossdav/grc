57b43bfa525a78b295580a5aefbfe42cfa0ec2cd
tiossdav <tiossdac@gmail.com>
2026-01-08 11:14:13 +0100

    initial

--- src/lib/images.ts
+++
@@ -1,9 +0,0 @@
-const IMAGES = {
-  INTRO_IMAGE: new URL("../assets/images/intro-image.jpg", import.meta.url)
-    .href,
-  BUILDING_1: new URL("../assets/images/building-1.jpg", import.meta.url).href,
-  BUILDING_2: new URL("../assets/images/building-2.png", import.meta.url).href,
-  LINES: new URL("../assets/images/lines.svg", import.meta.url).href,
-};
-
-export default IMAGES;
