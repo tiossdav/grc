466c84523134d2e6ee88407cd028857562572cd6
tiossdav <tiossdac@gmail.com>
2026-01-08 11:22:04 +0100

    added vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-    plugins: [tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
-        },
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
+  },
 });
